39bf03cbb1fa18db91790f295446338066b997e4
Aleksey T. <learies@ya.ru>
2026-04-12 08:42:28 +0500

    Добавлена начальная конфигурация conftest файла

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+from typing import Generator
+
+import pytest
+from flask import Flask
+from flask.testing import FlaskClient
+
+from app import create_app
+from app.settings.config import TestingConfig
+
+
+@pytest.fixture
+def app() -> Generator[Flask, None, None]:
+    """Фикстура для создания приложения"""
+    app: Flask = create_app(TestingConfig)
+
+    with app.app_context():
+        yield app
+
+
+@pytest.fixture
+def client(app: Flask) -> FlaskClient:
+    """Фикстура для создания клиента"""
+    return app.test_client()
