9bbefc806907d4123cb74d474c4d414e8cf15d1c
Aleksey T. <learies@ya.ru>
2026-04-12 08:31:26 +0500

    Добавлена загрузка из переменныйх окружения и создании Flask приложения

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,23 @@
+from typing import Mapping
+
 from flask import Flask
 
 from app.settings.config import Config, load_config
+from app.settings.env import init_env
 
 
-def create_app(config_class: type[Config]) -> Flask:
+def create_app(
+    config_class: type[Config], *, env: Mapping[str, str] | None = None
+) -> Flask:
     """Создание приложения"""
+    # Инициализация переменных окружения
+    if env is not None:
+        init_env()
+
+    # Создание приложения
     app = Flask(__name__)
+
+    # Загрузка конфигурации
     app.config.from_object(load_config(config_class))
 
     return app
