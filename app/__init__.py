cf21e9effcc24bd725c7588d4e20dc2de0bfb58c
Aleksey T. <learies@ya.ru>
2026-04-12 08:26:17 +0500

    Добавлена конфигруация при создании приложения

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 
+from app.settings.config import Config, load_config
 
-def create_app() -> Flask:
+
+def create_app(config_class: type[Config]) -> Flask:
     """Создание приложения"""
     app = Flask(__name__)
+    app.config.from_object(load_config(config_class))
 
     return app
