9d8c12955d39ae1eaf94625fb16cd8d7ef1ba4d0
Aleksey T. <learies@ya.ru>
2026-04-16 17:50:49 +0500

    Добавлен метод get_by_ids

--- app/blueprints/account/users/repositories.py
+++ app/blueprints/account/users/repositories.py
@@ -17,3 +17,9 @@ class UserRepository(AccountRepository[UserModel]):
     def get_by_id(self, user_id: UUID) -> UserModel | None:
         """Возвращает пользователя по идентификатору."""
         return self.get_by_attribute("id", user_id)
+
+    def get_by_ids(self, user_ids: Sequence[UUID]) -> Sequence[UserModel]:
+        """Возвращает пользователей по идентификаторам."""
+        return self.session.scalars(
+            select(UserModel).where(UserModel.id.in_(user_ids))
+        ).all()
--- app/blueprints/account/users/services.py
+++ app/blueprints/account/users/services.py
@@ -12,23 +12,33 @@ from .repositories import UserRepository
 class UserService(AccountService[UserModel, UserRepository]):
     """Сервис для работы с пользователями."""
 
-    def _get_response_dto(self, user: UserModel) -> UserResponseDTO:
-        """Получение DTO для пользователя."""
-        return UserResponseDTO(
-            id=user.id,
-            is_admin=user.is_admin,
-            last_login=user.last_login.last_login if user.last_login else None,
-        )
-
     def get_all(self) -> Sequence[UserResponseDTO]:
         """Получение всех пользователей."""
         users = self.repository.get_all()
-        return [self._get_response_dto(user) for user in users]
+        return self._get_response_dto(users)
 
     def get_by_id(self, user_id: UUID) -> UserResponseDTO | None:
         """Получение пользователя по идентификатору."""
         user = self.repository.get_by_id(user_id)
-        return self._get_response_dto(user) if user is not None else None
+        return self._get_response_dto([user])[0] if user is not None else None
+
+    def get_by_ids(self, user_ids: Sequence[UUID]) -> Sequence[UserResponseDTO]:
+        """Получение пользователей по идентификаторам."""
+        users = self.repository.get_by_ids(user_ids)
+        return self._get_response_dto(users)
+
+    def _get_response_dto(
+        self, users: Sequence[UserModel]
+    ) -> Sequence[UserResponseDTO]:
+        """Получение DTO для пользователя."""
+        return [
+            UserResponseDTO(
+                id=user.id,
+                is_admin=user.is_admin,
+                last_login=user.last_login.last_login if user.last_login else None,
+            )
+            for user in users
+        ]
 
 
 def get_user_service() -> UserService:
