ca5bb95e6cd8122dfdf25d535e4294ecc7db900e
Aleksey T. <learies@ya.ru>
2026-04-16 11:41:13 +0500

    Добавлен account blueprint

--- app/blueprints/__init__.py
+++ app/blueprints/__init__.py
@@ -1,5 +1,8 @@
 from flask import Flask
 
+from .account import account_bp
+
 
 def register_blueprints(app: Flask) -> None:
     """Регистрация блюпринтов."""
+    app.register_blueprint(account_bp)
--- app/blueprints/account/__init__.py
+++ app/blueprints/account/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 
-account_bp = Blueprint("account", __name__, url_prefix="/api/v1/account")
+account_bp = Blueprint("account", __name__, url_prefix="/api/v1/")
+
+from .users import api  # noqa
 
 __all__ = ("account_bp",)
