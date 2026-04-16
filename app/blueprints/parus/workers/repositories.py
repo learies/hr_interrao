0993fd0da2a73694807557c6ac2171a5ddfa2276
Aleksey T. <learies@ya.ru>
2026-04-16 19:09:30 +0500

    Добавлен count_all

--- app/blueprints/parus/workers/repositories.py
+++ app/blueprints/parus/workers/repositories.py
@@ -1,7 +1,8 @@
 from typing import Sequence
 from uuid import UUID
 
-from sqlalchemy import select
+from sqlalchemy import func, select
+from sqlalchemy.sql.selectable import Select
 
 from ..repositories import ParusRepository
 from .models import WorkerModel
@@ -10,10 +11,26 @@ from .models import WorkerModel
 class WorkerRepository(ParusRepository[WorkerModel]):
     """Репозиторий для работы с сотрудниками."""
 
-    def get_all(self) -> Sequence[WorkerModel]:
+    def get_all(
+        self, offset: int, limit: int, active: bool | None
+    ) -> Sequence[WorkerModel]:
         """Возвращает всех сотрудников."""
-        return self.session.scalars(select(WorkerModel)).all()
+        stmt = select(self.model)
+        stmt = self._apply_active(stmt, active)
+        return self.session.scalars(stmt.offset(offset).limit(limit)).all()
 
     def get_by_id(self, id: UUID) -> WorkerModel | None:
         """Возвращает сотрудника по идентификатору."""
         return self.get_by_attribute("id", id)
+
+    def count_all(self, active: bool | None) -> int:
+        """Возвращает количество всех сотрудников."""
+        stmt = select(func.count()).select_from(self.model)
+        stmt = self._apply_active(stmt, active)
+        return self.session.scalar(stmt) or 0
+
+    def _apply_active(self, stmt: Select, active: bool | None) -> Select:
+        """Применяет фильтр по активности."""
+        if active is not None:
+            return stmt.where(self.model.active.is_(active))
+        return stmt
